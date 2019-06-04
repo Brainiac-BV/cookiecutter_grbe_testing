6da3f30b4af51b2b5271604b2bec8957ed087b35
Kay <k.dgray23@gmail.com>
2019-06-04 12:13:51 -0700

    added admin and domain to email section

--- config/settings/production.py
+++ config/settings/production.py
@@ -117,7 +117,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     'DJANGO_DEFAULT_FROM_EMAIL',
-    default='testicles <noreply@example.com>'
+    default='Admin <noreply@keithdgray.com>'
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
